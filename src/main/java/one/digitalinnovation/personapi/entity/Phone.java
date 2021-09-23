6b16c3fc8574ee70cd86c27fe2b42824b120946b
lucaslgr-linux <lucaslgr1206@gmail.com>
2021-09-22 21:54:01 -0300

    Sets the JPA annotations to atributes of Phone class

--- src/main/java/one/digitalinnovation/personapi/entity/Phone.java
+++ src/main/java/one/digitalinnovation/personapi/entity/Phone.java
@@ -6,14 +6,23 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import one.digitalinnovation.personapi.enums.PhoneType;
 
+import javax.persistence.*;
+
+@Entity
 @Data
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
 public class Phone {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) //Setando que a chave ID vai ser provida pelo BANCO
     private Long id;
 
+    @Enumerated(EnumType.STRING) //Setando relacionamento com EnumType
+    @Column(nullable = false) //Setando NOT NULL
     private PhoneType type;
 
+    @Column(nullable = false) //Setando NOT NULL
     private String number;
 }
