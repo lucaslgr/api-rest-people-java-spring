d2c6e7e5c2ce1f01cf76d5bb3cd6a02d87b28261
lucaslgr-linux <lucaslgr1206@gmail.com>
2021-09-22 21:10:55 -0300

    Create the Phone entity class

---
+++ src/main/java/one/digitalinnovation/personapi/entity/Phone.java
@@ -0,0 +1,18 @@
+package one.digitalinnovation.personapi.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Phone {
+    private Long id;
+
+    private PhoneType type;
+
+    private String number;
+}
