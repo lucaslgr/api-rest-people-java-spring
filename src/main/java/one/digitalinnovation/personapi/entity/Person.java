1c053fea909d6ed17cb4bdcc9b378f2b48e1f922
lucaslgr-linux <lucaslgr1206@gmail.com>
2021-09-22 22:03:34 -0300

    Implements the JPA annotions in class Person

--- src/main/java/one/digitalinnovation/personapi/entity/Person.java
+++ src/main/java/one/digitalinnovation/personapi/entity/Person.java
@@ -5,24 +5,40 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.*;
 import java.time.LocalDate;
 import java.util.List;
 
+@Entity
 @Data
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
 public class Person {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(nullable = false)
     private String firstName;
 
+    @Column(nullable = false)
     private String lastname;
 
+    @Column(nullable = false, unique = true)
     private String cpf;
 
+    @Column(nullable = false)
     private LocalDate birthDate;
 
+    //FetchType.LAZY faz com que só seja buscado no banco a lista de telefones quando ela em específico for requerida
+    @OneToMany(
+            fetch = FetchType.LAZY,
+            cascade = {
+                    CascadeType.PERSIST,
+                    CascadeType.MERGE,
+                    CascadeType.REMOVE
+            })
     private List<Phone> phones;
 }
